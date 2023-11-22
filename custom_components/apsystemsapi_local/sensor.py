17222d9ef59868f1768557aaad8077f18a41c1c5
Mawoka <git@mawoka.eu>
2023-11-22 15:05:20 +0100

    :sparkles: Added more sensors

--- custom_components/apsystemsapi_local/sensor.py
+++ custom_components/apsystemsapi_local/sensor.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
+
+import asyncio
+from dataclasses import dataclass
+from collections.abc import Callable, Coroutine
 from aiohttp import client_exceptions
+
 import voluptuous as vol
+
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
@@ -14,6 +20,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from .apsystems_local_api import APsystemsEZ1M
+from homeassistant.helpers.device_registry import DeviceInfo
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_IP_ADDRESS): cv.string,
@@ -32,6 +39,16 @@ async def async_setup_platform(
 
     sensors = [
         PowerSensorTotal(api, device_name=config[CONF_NAME], sensor_name="Total Power", sensor_id="total_power"),
+        Panel1PowerSensor(api, device_name=config[CONF_NAME], sensor_name="Panel 1 Power", sensor_id="panel_1_power"),
+        Panel2PowerSensor(api, device_name=config[CONF_NAME], sensor_name="Panel 2 Power", sensor_id="panel_2_power"),
+        Panel1LifetimeEnergy(api, device_name=config[CONF_NAME], sensor_name="Panel 1 Lifetime Production",
+                             sensor_id="panel_1_lifetime_production"),
+        Panel2LifetimeEnergy(api, device_name=config[CONF_NAME], sensor_name="Panel 2 Lifetime Production",
+                             sensor_id="panel_2_lifetime_production"),
+        Panel1TodayEnergy(api, device_name=config[CONF_NAME], sensor_name="Panel 1 Today Production",
+                          sensor_id="panel_1_today_production"),
+        Panel2TodayEnergy(api, device_name=config[CONF_NAME], sensor_name="Panel 2 Today Production",
+                          sensor_id="panel_2_today_production"),
         LifetimeEnergy(api, device_name=config[CONF_NAME], sensor_name="Lifetime Production",
                        sensor_id="lifetime_production"),
         TodayEnergy(api, device_name=config[CONF_NAME], sensor_name="Today Production",
@@ -42,6 +59,7 @@ async def async_setup_platform(
 
 class BaseSensor(SensorEntity):
     """Representation of an APsystem sensor."""
+    _attr_available = True
 
     def __init__(self, api: APsystemsEZ1M, device_name: str, sensor_name: str, sensor_id: str):
         """Initialize the sensor."""
@@ -65,13 +83,26 @@ class BaseSensor(SensorEntity):
     def unique_id(self) -> str | None:
         return f"apsystemsapi_{self._device_name}_{self._sensor_id}"
 
+    @property
+    def device_info(self) -> DeviceInfo:
+        return DeviceInfo(
+            identifiers={
+                ("apsystemsapi_local", self._device_name)
+            },
+            name=self._device_name,
+            manufacturer="APsystems",
+            model="EZ1-M",
+        )
 
-class PowerSensorTotal(BaseSensor):
+
+class BasePowerSensor(BaseSensor):
     _device_class = SensorDeviceClass.POWER
     _attr_native_unit_of_measurement = UnitOfPower.WATT
     _attr_device_class = SensorDeviceClass.POWER
     _attr_state_class = SensorStateClass.MEASUREMENT
 
+
+class PowerSensorTotal(BasePowerSensor):
     async def async_update(self):
         try:
             self._state = await self._api.get_total_output()
@@ -80,10 +111,31 @@ class PowerSensorTotal(BaseSensor):
             self._attr_available = False
 
 
-class LifetimeEnergy(BaseSensor):
+class Panel1PowerSensor(BasePowerSensor):
+    async def async_update(self):
+        try:
+            self._state = (await self._api.get_output_data()).p1
+            self._attr_available = True
+        except client_exceptions.ClientConnectionError:
+            self._attr_available = False
+
+
+class Panel2PowerSensor(BasePowerSensor):
+    async def async_update(self):
+        try:
+            self._state = (await self._api.get_output_data()).p2
+            self._attr_available = True
+        except client_exceptions.ClientConnectionError:
+            self._attr_available = False
+
+
+class BaseEnergySensor(BaseSensor):
     _device_class = SensorDeviceClass.ENERGY
     _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
     _attr_device_class = SensorDeviceClass.ENERGY
+
+
+class LifetimeEnergy(BaseEnergySensor):
     _attr_state_class = SensorStateClass.TOTAL
 
     async def async_update(self):
@@ -94,10 +146,51 @@ class LifetimeEnergy(BaseSensor):
             self._attr_available = False
 
 
-class TodayEnergy(BaseSensor):
-    _device_class = SensorDeviceClass.ENERGY
-    _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
-    _attr_device_class = SensorDeviceClass.ENERGY
+class Panel1LifetimeEnergy(BaseEnergySensor):
+    _attr_state_class = SensorStateClass.TOTAL
+
+    async def async_update(self):
+        try:
+            self._state = (await self._api.get_output_data()).te1
+            self._attr_available = True
+        except client_exceptions.ClientConnectionError:
+            self._attr_available = False
+
+
+class Panel2LifetimeEnergy(BaseEnergySensor):
+    _attr_state_class = SensorStateClass.TOTAL
+
+    async def async_update(self):
+        try:
+            self._state = (await self._api.get_output_data()).te2
+            self._attr_available = True
+        except client_exceptions.ClientConnectionError:
+            self._attr_available = False
+
+
+class Panel1TodayEnergy(BaseEnergySensor):
+    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+
+    async def async_update(self):
+        try:
+            self._state = (await self._api.get_output_data()).e1
+            self._attr_available = True
+        except client_exceptions.ClientConnectionError:
+            self._attr_available = False
+
+
+class Panel2TodayEnergy(BaseEnergySensor):
+    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+
+    async def async_update(self):
+        try:
+            self._state = (await self._api.get_output_data()).e2
+            self._attr_available = True
+        except client_exceptions.ClientConnectionError:
+            self._attr_available = False
+
+
+class TodayEnergy(BaseEnergySensor):
     _attr_state_class = SensorStateClass.TOTAL_INCREASING
 
     async def async_update(self):
